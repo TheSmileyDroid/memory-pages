bb8cd8f2b32badea0b66b56c6c817ce9e61f2431
Gabriel H. Silva <42482192+TheSmileyDroid@users.noreply.github.com>
2023-05-25 04:03:08 +0000

    pages

--- pages.c
+++ pages.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define RAM_SIZE 1024 * 1024 * 16 // 16 MB
-#define PAG_SIZE 4096
+#define RAM_SIZE 1024
+#define PAG_SIZE 32
+#define NUM_PAGES (RAM_SIZE / PAG_SIZE)
 
-struct paget
-{
+struct paget {
     unsigned char referenciada : 1;
     unsigned char modificada : 1;
     unsigned char pres_aus : 1;
@@ -12,32 +14,56 @@ struct paget
     unsigned short moldura_pag;
     unsigned char mru_count;
     unsigned short ws_tempo_virtual;
-    // ...
 };
 
-int main()
-{
-    int num_pages = RAM_SIZE / PAG_SIZE;
-    struct paget page_table[num_pages];
+int main() {
+    struct paget page_table[NUM_PAGES] = {0};
     int next_page = 0;
+    int page_faults = 0;
 
-    // Access memory randomly
-    for (int i = 0; i < 1000000; i++)
-    {
+    srand(time(NULL));
+
+    for (int i = 0; i < 1000000; i++) {
         int addr = rand() % RAM_SIZE;
         int page_num = addr / PAG_SIZE;
-        if (!page_table[page_num].pres_aus)
-        {
+
+        if (!page_table[page_num].pres_aus) {
             // Page fault
+            page_faults++;
+
+            // Evict page using MRU algorithm
+            int mru_page = 0;
+            for (int j = 1; j < NUM_PAGES; j++) {
+                if (page_table[j].pres_aus && page_table[j].mru_count > page_table[mru_page].mru_count) {
+                    mru_page = j;
+                }
+            }
+
+            // Replace page
+            page_table[mru_page].pres_aus = 0;
             page_table[page_num].pres_aus = 1;
             page_table[page_num].moldura_pag = next_page++;
-            if (next_page == num_pages)
-            {
-                next_page = 0;
+            page_table[page_num].mru_count = 0;
+            page_table[page_num].ws_tempo_virtual = i;
+        }
+
+        // Update MRU count
+        for (int j = 0; j < NUM_PAGES; j++) {
+            if (page_table[j].pres_aus) {
+                page_table[j].mru_count++;
             }
         }
-        page_table[page_num].referenciada = 1;
+        page_table[page_num].mru_count = 0;
+
+        // Update WS timestamp
+        for (int j = 0; j < NUM_PAGES; j++) {
+            if (page_table[j].pres_aus) {
+                page_table[j].ws_tempo_virtual = i - page_table[j].mru_count;
+            }
+        }
     }
+
+    printf("Page faults: %d\n", page_faults);
 
     return 0;
 }
