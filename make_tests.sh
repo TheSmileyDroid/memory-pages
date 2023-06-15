bd37c2da44aa70085e86a81e879738f097e5a3e9
Gabriel H. Silva <42482192+TheSmileyDroid@users.noreply.github.com>
2023-06-15 18:21:39 +0000

    quase pronto

--- make_tests.sh
+++ make_tests.sh
@@ -4,6 +4,7 @@ gcc mmu.c -o mmu -lm
 # mmu <algoritmo> <% de desvio padrão> <acessos por interrupção> <seed> <num de interrupções> <tau>
 # alg: c = clock, w = working set, a = aging
 echo "Executando..."
-./mmu c 10 3000 1 10
-./mmu w 10 3000 1 10 2000
-./mmu a 10 300 1 10
+time ./mmu w 10 3000 1 1000 
+time ./mmu w 10 3000 1 1000 
+time ./mmu w 10 3000 1 1000 
+time ./mmu w 10 3000 1 1000 
